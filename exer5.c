b34f340b7d1802ff0db326a404bc5f4896681e55
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-08 23:08:23 -0300

    Update exer5.c

--- exer5.c
+++ exer5.c
@@ -18,11 +18,11 @@ void main() {
         printf("maior:%.2f\nmeio:%.2f\nmenor:%.2f", v2, v3, v1);
     }else if((v3 > v2) && (v2 > v1)){
         printf("maior:%.2f\nmeio:%.2f\nmenor:%.2f", v3, v2, v1);
-    }else if (v3 > v1 && v1 > v2){
+    }else if ((v3 > v1) && (v1 > v2)){
         printf("maior:%.2f\nmeio:%.2f\nmenor:%.2f", v3, v1, v2);
-    }else if (v1 > v3 && v3 > v2){
+    }else if ((v1 > v3) && (v3 > v2)){
         printf("maior:%.2f\nmeio:%.2f\nmenor:%.2f", v1, v3, v2);
-    }else if(v2 > v1 && v1 > v3){
+    }else if((v2 > v1) && (v1 > v3)){
         printf("maior:%.2f\nmeio:%.2f\nmenor:%.2f", v2, v1, v3);
     }
 }
