6a0c9ad2c35941abaa8e23a5bfaea3a936f031c8
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-09 17:21:57 -0300

    Update exer6.c

--- exer6.c
+++ exer6.c
@@ -7,24 +7,24 @@ void main() {
     printf("Digite o operador: ");
     scanf("%c", &operador);
 
-    printf("Digite o número 1: ");
+    printf("Digite o nÃºmero 1: ");
     scanf("%d", &num1);
 
-    printf("Digite o número 2: ");
+    printf("Digite o nÃºmero 2: ");
     scanf("%d", &num2);
 
     switch(operador){
     case '+':
-        printf("%d + %d = %d",num1,num2, num1+num2);
+        printf("%d + %d = %d",num1,num2,num1+num2);
         break;
     case '-':
-        printf("%d - %d = %d",num1,num2, num1-num2);
+        printf("%d - %d = %d",num1,num2,num1-num2);
         break;
     case '*':
-        printf("%d x %d = %d",num1,num2, num1*num2);
+        printf("%d x %d = %d",num1,num2,num1*num2);
         break;
     case '/':
-        printf("%d / %d = %d",num1,num2, num1/num2);
+        printf("%d / %d = %d",num1,num2,num1/num2);
         break;
     }
 }
