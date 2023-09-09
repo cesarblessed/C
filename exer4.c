9c7a752b8afa2f704e0d5fd4cb1935846cc38c38
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-08 23:10:14 -0300

    Update exer4.c

--- exer4.c
+++ exer4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <locale.h> //biblioteca para formatar padr„o do paÌs
+#include <locale.h> //biblioteca para formatar padr√£o do pa√≠s
 
 void main() {
-    setlocale(LC_ALL, "Portuguese"); //No meu caso sou brasileiro
+    setlocale(LC_ALL, "Portuguese"); //No meu caso sou brasileiro(portugu√™s)
     float x, y, z;
 
     printf("Digite o valor x: ");
@@ -15,10 +15,10 @@ void main() {
     scanf("%f", &z);
 
     if(x == y && y == z){
-        printf("Equil·tero");
+        printf("Equil√°tero");
     }else if(x!=y && x!=z && y!=z){
         printf("Escaleno");
     }else{
-        printf("isÛsceles");
+        printf("is√≥sceles");
     }
 }
