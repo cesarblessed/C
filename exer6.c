418670081fc1d09973cfe169238a482b06c61917
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-09 17:20:49 -0300

    sexto commit(calculadora com operador)
    usuário digita dois números e um operador, e o código retorna o resultado

---
+++ exer6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main() {
+    int num1, num2;
+    char operador;
+
+    printf("Digite o operador: ");
+    scanf("%c", &operador);
+
+    printf("Digite o número 1: ");
+    scanf("%d", &num1);
+
+    printf("Digite o número 2: ");
+    scanf("%d", &num2);
+
+    switch(operador){
+    case '+':
+        printf("%d + %d = %d",num1,num2, num1+num2);
+        break;
+    case '-':
+        printf("%d - %d = %d",num1,num2, num1-num2);
+        break;
+    case '*':
+        printf("%d x %d = %d",num1,num2, num1*num2);
+        break;
+    case '/':
+        printf("%d / %d = %d",num1,num2, num1/num2);
+        break;
+    }
+}
+
