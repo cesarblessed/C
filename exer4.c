c6bc07eb76ae02076e40561ba104d1d8015f929b
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-08 23:02:55 -0300

    commit 4(triangulo)
    usuário digita 3 valores referente aos lados do triângulo, e o código retorna se é equilátero, isósceles ou escaleno

---
+++ exer4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h> //biblioteca para formatar padrão do país
+
+void main() {
+    setlocale(LC_ALL, "Portuguese"); //No meu caso sou brasileiro
+    float x, y, z;
+
+    printf("Digite o valor x: ");
+    scanf("%f", &x);
+
+    printf("Digite o valor y: ");
+    scanf("%f", &y);
+
+    printf("Digite o valor z: ");
+    scanf("%f", &z);
+
+    if(x == y && y == z){
+        printf("Equilátero");
+    }else if(x!=y && x!=z && y!=z){
+        printf("Escaleno");
+    }else{
+        printf("isósceles");
+    }
+}
