cfa251d0cbdf4ae903a86075859eb0347cbf9ab3
Cesar Augusto de Paula <cesardepaula80@gmail.com>
2023-09-11 14:07:15 -0300

    Update exer9.c

--- exer9.c
+++ exer9.c
@@ -5,25 +5,35 @@ void main(){
 
 char tipoAno[20];
 
-
     int mes;
-
-
 
     printf("Digite um mês: ");
     scanf("%d", &mes);
 
-     printf("Digite tipo de ano: ");
+    printf("Digite tipo de ano: ");
     scanf("%s", tipoAno);
 
 
     if (mes == 2 && (strcmp(tipoAno, "b") == 0)) {
         printf("Fevereiro - 29 dias\n");
+    }else{
+        switch(mes){
+        case 1:
+            printf("Janeiro - 31 dias");
+            break;
+        case 2:
+            printf("Fevereiro - 28 dias");
+            break;
+        case 3:
+            printf("Março - 31 dias");
+            break;
+        case 4:
+            printf("Abril - 30 dias");
+            break;
+        case 5:
+            printf("Maio - 31 dias");
+            break;
+
     }
-
-    switch(mes){
-
-    }
-
-
 }
+
